4650778d13d99285bafcfc438fe4b8e4a635498b
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-25 11:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //your JS code here. If required.
+Object.prototype=getKeys(){
+	let arr=[]
+	for(let i=0;i<this.length;i++)
+		{
+			arr.push(this[i]);
+		}
+	return arr;
+}
+
 let student{
 	name:"abc";
 	
 }
+
 student.getKeys();
